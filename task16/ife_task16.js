3cde382d2630b5536e55a51cd336fc1f328b5e48
Charlotte <zuiaicharlotte@gmail.com>
2016-03-23 16:17:20 +0800

    first commit

--- task16/ife_task16.js
+++ task16/ife_task16.js
@@ -8,12 +8,22 @@
  */
 var aqiData = {};
 
+var aqiCity = document.getElementById("aqi-city-input").value;
+var aqiValue = document.getElementById("aqi-value-input").value;
+
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-
+	if (aqiCity.match(/[\u4e00-\u9fa5]/g)){
+		console.log("hi");
+		return true
+	} else {
+		aqiCity = "";
+		alert("请输入中文");
+		console.log("opus");
+	}
 }
 
 /**
@@ -45,7 +55,10 @@ function delBtnHandle() {
 function init() {
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+  addBtn = document.getElementById("add-btn");
+  if(addBtn){
+  	addBtn.addEventListener("click",addBtnHandle,false);
+  }
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }

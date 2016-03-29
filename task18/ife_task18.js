86fcfac04867fec095b5f2bc5772656a552dc84c
7Charlotte <zuiaicharlotte@gmail.com>
2016-03-29 22:56:54 +0800

    fIx renderList

--- task18/ife_task18.js
+++ task18/ife_task18.js
@@ -1,46 +1,83 @@
 var numberList = new Array();
 
 //验证数字
-function checkNumber(){
-	var inputNumber = document.getElementById("number-input").value;
-	if(/^[0-9]+$/.test(inputNumber) != true) {
-		alert("请输入数字");
-		return;
-	} else{
-		return inputNumber;
-	}
-	
+function checkNumber() {
+    var inputNumber = document.getElementById("number-input").value;
+    if (/^[0-9]+$/.test(inputNumber) != true) {
+        alert("请输入数字");
+        return;
+    } else {
+        return inputNumber;
+    }
+
 }
 
-
-function renderList(){
-	var outputNumber = document.getElementById("number-output");
-	outputNumber.innerHTML = "";
-	for(var i = 0; i < numberList.length ; i++){
-		var numberItem = document.createElement("li");
-		numberItem.innerText = numberList[i];
-		outputNumber.appendChild(numberItem);
-	}
+function renderList() {
+    var outputNumber = document.getElementById("number-output");
+    outputNumber.innerHTML = "";
+    for (var i = 0; i < numberList.length; i++) {
+        var numberItem = document.createElement("li");
+        numberItem.innerText = numberList[i];
+        outputNumber.appendChild(numberItem);
+        if (numberItem) {
+            numberItem.addEventListener("click", function(){
+                this.parentNode.removeChild(this);
+                alert(this.innerText);
+                numberList.splice(i,1);
+            }, false);
+        }
+    }
 }
 
-function leftInHandle(){
-	if(checkNumber()){
-		numberList.push(checkNumber());
-	}
-	renderList();
+function leftInHandle() {
+    if (checkNumber()) {
+        numberList.unshift(checkNumber());
+    }
+    renderList();
 }
 
-function rightIn(){}
+function rightInHandle() {
+    if (checkNumber()) {
+        numberList.push(checkNumber());
+    }
+    renderList();
+}
 
-function leftOut(){}
+function leftOutHandle() {
+    var item = numberList.shift();
+    renderList();
+    alert(item);
+}
 
-function rightOut(){}
+function rightOutHandle() {
+    var item = numberList.pop();
+    renderList();
+    alert(item);
+}
 
+function delItemHandle(i) {
+    delete numberList[i];
+    console.log(numberList);
 
-function init(){
-	var leftInBtn = document.getElementById("left-in");
-	if (leftInBtn) {
+}
+
+function init() {
+    var leftInBtn = document.getElementById("left-in");
+    var rightInBtn = document.getElementById("right-in");
+    var leftOutBtn = document.getElementById("left-out");
+    var rightOutBtn = document.getElementById("right-out");
+
+    if (leftInBtn) {
         leftInBtn.addEventListener("click", leftInHandle, false);
+    }
+    if (rightInBtn) {
+        rightInBtn.addEventListener("click", rightInHandle, false);
+    }
+    if (leftOutBtn) {
+        leftOutBtn.addEventListener("click", leftOutHandle, false);
+    }
+    if (rightOutBtn) {
+        rightOutBtn.addEventListener("click", rightOutHandle, false);
     }
 }
 init();

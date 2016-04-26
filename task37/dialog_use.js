fba61cb96f1d15c2d46520095ed081cfc5af0079
Charlotte <zuiaicharlotte@gmail.com>
2016-04-26 15:56:13 +0800

    fix task37

--- task37/dialog_use.js
+++ task37/dialog_use.js
@@ -6,29 +6,45 @@ var trigBtn = document.getElementById("trig-btn");
 var mask = document.querySelector("#mask");
 var dialogBtn = document.querySelectorAll("#dialog-box button");
 
+var layer = new Layer(dialogBox);
+layer.maskEle = mask;
 
-trigBtn.addEventListener("click", showHandler, false);
-mask.addEventListener("click", hideHandler, false);
+trigBtn.addEventListener("click", layer.showHandler, false);
+mask.addEventListener("click", layer.hideHandler, false);
 for (var i = 0; i < dialogBtn.length; ++i) {
-    dialogBtn[i].addEventListener("click", hideHandler, false);
+    dialogBtn[i].addEventListener("click", layer.hideHandler, false);
 }
 
 
-function showHandler() {
-    mask.style.display = "block";
-    dialogBox.style.display = "block";
-    dialogBox.style.top = (document.documentElement.clientHeight - dialogBox.offsetHeight) / 2 + "px";
-    dialogBox.style.left = (document.documentElement.clientWidth - dialogBox.offsetWidth) / 2 + "px";
-}
 
-function hideHandler() {
-    mask.style.display = "none";
-    dialogBox.style.display = "none";
-}
+var Layer = function(ele){
 
+	this.maskEle = null,
 
-window.onscroll = function (e) {
+	showHandler: function(){
+		maskEle.style.display = "block";
+	    ele.style.display = "block";
+	    ele.style.top = (document.documentElement.clientHeight - ele.offsetHeight) / 2 + "px";
+	    ele.style.left = (document.documentElement.clientWidth - ele.offsetWidth) / 2 + "px";
+
+	},
+
+	hideHandler: function(){
+		maskEle.style.display = "none";
+   		ele.style.display = "none";
+	},
+
+	window.onscroll = function () {
     // 当页面的滚动条滚动时,会执行这里的代码
-    dialogBox.style.top = (document.documentElement.clientHeight - dialogBox.offsetHeight ) / 2 + document.documentElement.scrollTop + "px";
+	    if (ele.display == "block") {
+	    	ele.style.top = (document.documentElement.clientHeight - ele.offsetHeight ) / 2 + document.documentElement.scrollTop + "px";
+	    }
+	}
+
 }
+
+
+
+
+
 

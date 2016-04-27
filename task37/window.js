2374107873e211a2f1746d918adb1e61080738cd
Charlotte <zuiaicharlotte@gmail.com>
2016-04-27 12:57:44 +0800

    fix task37
    document.documentElement.scrollTop
    某些chrome版本不支持。要用document.body.scrollTop

--- task37/window.js
+++ task37/window.js
@@ -2,8 +2,8 @@
  * Created by liuxia on 16/4/26.
  */
 function scroll(ele) {
-    console.log("scroll layer");
+    console.log("scroll layer"+" param"+ ele.clientHeight);
     if (ele.style.display == "block") {
-        ele.style.top = (document.documentElement.clientHeight - ele.offsetHeight ) / 2 + document.documentElement.scrollTop + "px";
-    }
+    	ele.style.top = (document.documentElement.clientHeight - ele.offsetHeight ) / 2 +(document.documentElement.scrollTop || document.body.scrollTop )+ "px";
+    } 
 }

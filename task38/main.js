d330be05094fb8f453aa519cf21f45e3083f3417
7Charlotte <zuiaicharlotte@gmail.com>
2016-04-29 22:40:40 +0800

    dev task38
    有数据输入接口和方法扩展接口

--- task38/main.js
+++ task38/main.js
@@ -1,7 +1,9 @@
 var score1 = new Score("王小明", 80, 90, 95);
 var score2 = new Score("张红", 70, 87, 83);
 var score3 = new Score("李刚", 93, 94, 83);
+var score4 = new Score("向宇波", 100, 98, 99);
 var scoreArr = [score1, score2, score3];
+scoreArr.push(score4);
 init();
 
 
@@ -17,7 +19,7 @@ function Score(name, chinese, math, english) {
 function renderTable(arr) {
     var tbody = document.querySelector("tbody");
     tbody.innerHTML = "";
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < scoreArr.length; i++) {
         var tr = document.createElement("tr");
         var data = new Array();
         data.push("<td>" + arr[i].name + "</td>" + "<td>" + arr[i].chinese + "</td>" + "<td>" + arr[i].math + "</td>" + "<td>" + arr[i].english + "</td>" + "<td>" + arr[i].sum + "</td>");
@@ -26,6 +28,8 @@ function renderTable(arr) {
     }
 }
 
+
+/*排序的各种方法,目前只有降序*/
 function campareChinese(o1, o2) {
     return o1.chinese > o2.chinese; //改变符号即可变为升序;
 }
@@ -41,12 +45,13 @@ function campareEnglish(o1, o2) {
 function campareSum(o1, o2) {
     return o1.sum > o2.sum;
 }
+/*--------*/
 
 function sortNumber(arr, func) { //选择排序
     for (var i = 0; i < arr.length; i++) {
         var min = i;
         for (var j = i + 1; j < arr.length; j++) {
-            if (func(arr[j], arr[min])) { //将数据类型抽象出来
+            if (func(arr[j], arr[min])) { //将数据类型抽象出来,方法可以在排序方法模块中扩展;
                 min = j;
             }
         }
@@ -57,18 +62,18 @@ function sortNumber(arr, func) { //选择排序
 }
 
 function btnHandler(e) {
-    switch (e.target.id){
+    switch (e.target.id) {
         case "Chinese":
             sortNumber(scoreArr, campareChinese);
             break;
         case "Math":
-            sortNumber(scoreArr,campareMath);
+            sortNumber(scoreArr, campareMath);
             break;
         case "English":
-            sortNumber(scoreArr,campareEnglish);
+            sortNumber(scoreArr, campareEnglish);
             break;
         case "Sum":
-            sortNumber(scoreArr,campareSum);
+            sortNumber(scoreArr, campareSum);
             break;
     }
     renderTable(scoreArr);
@@ -79,7 +84,7 @@ function init() {
     renderTable(scoreArr);
     var buttonArr = document.querySelectorAll("button");
     for (var i = 0; i < buttonArr.length; i++) {
-        buttonArr[i].addEventListener("click",btnHandler, false);
+        buttonArr[i].addEventListener("click", btnHandler, false);
     }
 }
 
